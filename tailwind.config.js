6e281d2d28a1e3899229c81bfcf820ac41f270c8
Tony Sanjuan <tonysanj13@gmail.com>
2022-01-29 12:29:23 +0100

    feature:added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/app/components/**/*.{html,ts}",
+    "./src/app/pages/**/*.{html,ts}",
+    "./src/app/layouts/**/*.{html,ts}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
